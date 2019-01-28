40fcc9887e4b1ced44964cc161e191fd005107e1
sergei.nechaenko <sergei.nechaenko@i-media.ua>
2019-01-28 11:19:46 +0200

    ADD: option to curl

--- src/TelegramChat.php
+++ src/TelegramChat.php
@@ -48,6 +48,7 @@ class TelegramChat
             curl_setopt($ch, CURLOPT_URL, $url);
             curl_setopt($ch, CURLOPT_TIMEOUT, $timeout);
             curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 
             curl_exec($ch);
             curl_close($ch);
